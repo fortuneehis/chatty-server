043b1e7dd1de5504ab46887fa7eddc55c7e8a95b
Fortune Ehis <ehiscodefox@gmail.com>
2022-05-09 17:27:46 +0100

    Updates

--- src/services/message.service.ts
+++ src/services/message.service.ts
@@ -2,9 +2,6 @@ import { Message } from "../dtos"
 import prismaClient from "../db"
 
 
-
-
-
 export const addMessage = async(chatId: number, {message, senderId, voiceMessageAudioPath, isVoiceMessage}: Message): Promise<[Message|null, unknown]> => {
     try {
         const newMessage = await prismaClient.message.create({
@@ -26,6 +23,28 @@ export const addMessage = async(chatId: number, {message, senderId, voiceMessage
         })
 
         return [newMessage, null]
+    } catch(err) {
+        return [null, err]
+    }
+}
+
+export const getMessages = async(chatId: number) => {
+    try {
+        //add createdAt column
+        const messages = await prismaClient.message.groupBy({
+            by: ["chatId"], //change to createdAt
+            orderBy: {
+               chatId: "asc"
+            },
+            where: {
+                chat: {
+                    id: chatId
+                }
+            }
+        })
+
+        return [messages, null]
+
     } catch(err) {
         return [null, err]
     }
--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,6 +1,7 @@
 import {Server} from 'socket.io'
 import { User } from '../dtos'
 import config from '../utils/config'
+import connection from '../utils/connections'
 import CustomError from '../utils/error'
 import { verifyJWT } from '../utils/user'
 import listeners from './listeners'
@@ -20,10 +21,25 @@ export default (io: Server)=> {
          if(error) {
             return next(error as Error)
         }
+
+        //@ts-ignore
+        socket.request.user = user
     })
    
     io.on("connection", socket=>{ 
+        //@ts-ignore
+        connection.add(socket.request.user.id, socket.id)
         listeners(io, socket)
+
+        socket.on("disconnect", ()=> {
+            //@ts-ignore
+            if(connection.exists(socket.request.user.id)) {
+                //@ts-ignore
+                connection.remove(socket.request.user.id)
+            }
+            //create an active users event and fetch active users based on their Id and send to client
+        })
     })
+
 
 } 
